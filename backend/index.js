fac64a1de1dfaae8219bc89380a39d2441158314
akhilomeella <akhilomeella@gmail.com>
2025-09-12 14:56:32 +0100

    added env file

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const sneakerModel = require("./models/Sneaker");
+const dotenv = require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  sneakerModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("Incorrect password");
+      }
+    } else {
+      res.json("Record does not exist");
+    }
+  });
+});
+
+app.post("/signup", (req, res) => {
+  sneakerModel
+    .create(req.body)
+    .then((register) => res.json(register))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3000, () => {
+  console.log("servr is running");
+});
