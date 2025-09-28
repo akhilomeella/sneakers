eeef56230915dd4e37730c28677d1a209f161e83
akhilomeella <akhilomeella@gmail.com>
2025-09-28 23:03:33 +0100

    moved the order history page from being a route to just being displayed as a content in the account page

--- client/src/Pages/Account.jsx
+++ client/src/Pages/Account.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import Sidebar from "../Components/Sidebar";
+import Orderhistory from "./Orderhistory";
+import ProtectedRoute from "../Components/ProtectedRoute";
 
 const Account = () => {
-  return <div>This is Account</div>;
+  const [activeSection, setActiveSection] = useState("dashboard");
+
+  return (
+    <div className=" flex ">
+      <div>
+        <Sidebar activeSection={activeSection} onNavigate={setActiveSection} />
+      </div>
+      <div className="w-full">
+        <ProtectedRoute>
+          {activeSection === "dashboard" && <h1>Dashboard</h1>}
+          {activeSection === "orders" && <Orderhistory />}
+          {activeSection === "support" && <h1>Support</h1>}
+        </ProtectedRoute>
+      </div>
+    </div>
+  );
 };
 
 export default Account;
