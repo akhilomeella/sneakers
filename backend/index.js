6f560b8cedd9112726a4aafc9dae7757be29d7e6
akhilomeella <akhilomeella@gmail.com>
2025-10-04 10:59:53 +0100

    make server accessible to to any device

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI);
 
