db9031b2520b6a148fd808a4a220745364cf967d
akhilomeella <akhilomeella@gmail.com>
2025-11-17 17:45:53 +0100

    added password hashing

--- backend/models/Sneaker.js
+++ backend/models/Sneaker.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const sneakerSchema = new mongoose.Schema({
   name: String,
-  email: String,
-  password: String,
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [/.+@.+\..+/, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [8, "Password must be at least 8 characters long"],
+  },
+});
+
+sneakerSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const sneakerModel = mongoose.model("register", sneakerSchema);
