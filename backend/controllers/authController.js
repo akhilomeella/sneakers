1b5bc8e82fb3fdf49d7d097595da621370d8d087
akhilomeella <akhilomeella@gmail.com>
2025-11-27 16:44:39 +0100

    created separate files for database connection, route and its functions

---
+++ backend/controllers/authController.js
@@ -0,0 +1,51 @@
+const sneakerModel = require("./models/Sneaker");
+const bcrypt = require("bcrypt");
+
+const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if user exists
+    const userExist = await sneakerModel.findOne({ email });
+    if (userExist) {
+      return res.status(400).json("User already exists");
+    }
+
+    // Hash password
+    const hash_password = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await sneakerModel.create({
+      name,
+      email,
+      password: hash_password,
+    });
+
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await sneakerModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json("Record does not exist");
+    }
+
+    // Compare password
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).json("Incorrect password or username");
+    }
+
+    res.json("Success");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = { signup, login };
